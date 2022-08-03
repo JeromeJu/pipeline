7574807a6e4c791f40fad8d8f66456bb34236164
Jerome Ju <jeromeju@google.com>
2022-08-03 15:09:10 +0000

    Add V1 TaskRun Golang structs
    This commit adds structs and validation for v1 TaskRun, including params,
    resultRef and workspaces. This is copied from v1beta1 with the following exceptions:
    
    - omitted TaskRunResources
       - This includes the `TaskRunInputs` and `TaskRunOutputs` and respective test
    - omitted `CloudEventDeliveryState`
    - renamed the Go fields of `StepState.ContainerName` to `StepState.Container`and
    `SidecarState.ContainerName` to `SidecarState.Container` but not the api fields
    - renamed `taskResults` to `results`

---
+++ pkg/apis/pipeline/v1/taskrun_validation_test.go
@@ -0,0 +1,515 @@
+/*
+Copyright 2022 The Tekton Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/tektoncd/pipeline/pkg/apis/config"
+	v1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
+	"github.com/tektoncd/pipeline/test/diff"
+	corev1 "k8s.io/api/core/v1"
+	corev1resources "k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/pkg/apis"
+)
+
+func TestTaskRun_Invalidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		taskRun *v1.TaskRun
+		want    *apis.FieldError
+	}{{
+		name:    "invalid taskspec",
+		taskRun: &v1.TaskRun{},
+		want: apis.ErrMissingOneOf("spec.taskRef", "spec.taskSpec").Also(
+			apis.ErrGeneric(`invalid resource name "": must be a valid DNS label`, "metadata.name")),
+	}}
+	for _, ts := range tests {
+		t.Run(ts.name, func(t *testing.T) {
+			err := ts.taskRun.Validate(context.Background())
+			if d := cmp.Diff(ts.want.Error(), err.Error()); d != "" {
+				t.Error(diff.PrintWantGot(d))
+			}
+		})
+	}
+}
+
+func TestTaskRun_Validate(t *testing.T) {
+	tests := []struct {
+		name    string
+		taskRun *v1.TaskRun
+		wc      func(context.Context) context.Context
+	}{{
+		name: "do not validate spec on delete",
+		taskRun: &v1.TaskRun{
+			ObjectMeta: metav1.ObjectMeta{Name: "taskrname"},
+		},
+		wc: apis.WithinDelete,
+	}, {
+		name: "alpha feature: valid step and sidecar overrides",
+		taskRun: &v1.TaskRun{
+			ObjectMeta: metav1.ObjectMeta{Name: "tr"},
+			Spec: v1.TaskRunSpec{
+				TaskRef: &v1.TaskRef{Name: "task"},
+				StepOverrides: []v1.TaskRunStepOverride{{
+					Name: "foo",
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{corev1.ResourceMemory: corev1resources.MustParse("1Gi")},
+					},
+				}},
+				SidecarOverrides: []v1.TaskRunSidecarOverride{{
+					Name: "bar",
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{corev1.ResourceMemory: corev1resources.MustParse("1Gi")},
+					},
+				}},
+			},
+		},
+		wc: config.EnableAlphaAPIFields,
+	}}
+	for _, ts := range tests {
+		t.Run(ts.name, func(t *testing.T) {
+			ctx := context.Background()
+			if ts.wc != nil {
+				ctx = ts.wc(ctx)
+			}
+			if err := ts.taskRun.Validate(ctx); err != nil {
+				t.Errorf("TaskRun.Validate() error = %v", err)
+			}
+		})
+	}
+}
+
+func TestTaskRun_Workspaces_Invalid(t *testing.T) {
+	tests := []struct {
+		name    string
+		tr      *v1.TaskRun
+		wantErr *apis.FieldError
+	}{{
+		name: "make sure WorkspaceBinding validation invoked",
+		tr: &v1.TaskRun{
+			ObjectMeta: metav1.ObjectMeta{Name: "taskname"},
+			Spec: v1.TaskRunSpec{
+				TaskRef: &v1.TaskRef{Name: "task"},
+				Workspaces: []v1.WorkspaceBinding{{
+					Name:                  "workspace",
+					PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{},
+				}},
+			},
+		},
+		wantErr: apis.ErrMissingField("spec.workspaces[0].persistentvolumeclaim.claimname"),
+	}, {
+		name: "bind same workspace twice",
+		tr: &v1.TaskRun{
+			ObjectMeta: metav1.ObjectMeta{Name: "taskname"},
+			Spec: v1.TaskRunSpec{
+				TaskRef: &v1.TaskRef{Name: "task"},
+				Workspaces: []v1.WorkspaceBinding{{
+					Name:     "workspace",
+					EmptyDir: &corev1.EmptyDirVolumeSource{},
+				}, {
+					Name:     "workspace",
+					EmptyDir: &corev1.EmptyDirVolumeSource{},
+				}},
+			},
+		},
+		wantErr: apis.ErrMultipleOneOf("spec.workspaces[1].name"),
+	}}
+	for _, ts := range tests {
+		t.Run(ts.name, func(t *testing.T) {
+			err := ts.tr.Validate(context.Background())
+			if err == nil {
+				t.Errorf("Expected error for invalid TaskRun but got none")
+			}
+			if d := cmp.Diff(ts.wantErr.Error(), err.Error()); d != "" {
+				t.Error(diff.PrintWantGot(d))
+			}
+		})
+	}
+}
+
+func TestTaskRunSpec_Invalidate(t *testing.T) {
+	invalidStatusMessage := "status message without status"
+	tests := []struct {
+		name    string
+		spec    v1.TaskRunSpec
+		wantErr *apis.FieldError
+		wc      func(context.Context) context.Context
+	}{{
+		name:    "invalid taskspec",
+		spec:    v1.TaskRunSpec{},
+		wantErr: apis.ErrMissingOneOf("taskRef", "taskSpec"),
+	}, {
+		name: "invalid taskref and taskspec together",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{
+				Name: "taskrefname",
+			},
+			TaskSpec: &v1.TaskSpec{
+				Steps: []v1.Step{{
+					Name:  "mystep",
+					Image: "myimage",
+				}},
+			},
+		},
+		wantErr: apis.ErrMultipleOneOf("taskRef", "taskSpec"),
+	}, {
+		name: "negative pipeline timeout",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{
+				Name: "taskrefname",
+			},
+			Timeout: &metav1.Duration{Duration: -48 * time.Hour},
+		},
+		wantErr: apis.ErrInvalidValue("-48h0m0s should be >= 0", "timeout"),
+	}, {
+		name: "wrong taskrun cancel",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{
+				Name: "taskrefname",
+			},
+			Status: "TaskRunCancell",
+		},
+		wantErr: apis.ErrInvalidValue("TaskRunCancell should be TaskRunCancelled", "status"),
+	}, {
+		name: "incorrectly set statusMesage",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{
+				Name: "taskrefname",
+			},
+			StatusMessage: v1.TaskRunSpecStatusMessage(invalidStatusMessage),
+		},
+		wantErr: apis.ErrInvalidValue(fmt.Sprintf("statusMessage should not be set if status is not set, but it is currently set to %s", invalidStatusMessage), "statusMessage"),
+	}, {
+		name: "invalid taskspec",
+		spec: v1.TaskRunSpec{
+			TaskSpec: &v1.TaskSpec{
+				Steps: []v1.Step{{
+					Name:  "invalid-name-with-$weird-char/%",
+					Image: "myimage",
+				}},
+			},
+		},
+		wantErr: &apis.FieldError{
+			Message: `invalid value "invalid-name-with-$weird-char/%"`,
+			Paths:   []string{"taskSpec.steps[0].name"},
+			Details: "Task step name must be a valid DNS Label, For more info refer to https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names",
+		},
+	}, {
+		name: "invalid params - exactly same names",
+		spec: v1.TaskRunSpec{
+			Params: []v1.Param{{
+				Name:  "myname",
+				Value: *v1.NewArrayOrString("value"),
+			}, {
+				Name:  "myname",
+				Value: *v1.NewArrayOrString("value"),
+			}},
+			TaskRef: &v1.TaskRef{Name: "mytask"},
+		},
+		wantErr: apis.ErrMultipleOneOf("params[myname].name"),
+	}, {
+		name: "invalid params - same names but different case",
+		spec: v1.TaskRunSpec{
+			Params: []v1.Param{{
+				Name:  "FOO",
+				Value: *v1.NewArrayOrString("value"),
+			}, {
+				Name:  "foo",
+				Value: *v1.NewArrayOrString("value"),
+			}},
+			TaskRef: &v1.TaskRef{Name: "mytask"},
+		},
+		wantErr: apis.ErrMultipleOneOf("params[foo].name"),
+	}, {
+		name: "invalid params (object type) - same names but different case",
+		spec: v1.TaskRunSpec{
+			Params: []v1.Param{{
+				Name:  "MYOBJECTPARAM",
+				Value: *v1.NewObject(map[string]string{"key1": "val1", "key2": "val2"}),
+			}, {
+				Name:  "myobjectparam",
+				Value: *v1.NewObject(map[string]string{"key1": "val1", "key2": "val2"}),
+			}},
+			TaskRef: &v1.TaskRef{Name: "mytask"},
+		},
+		wantErr: apis.ErrMultipleOneOf("params[myobjectparam].name"),
+		wc:      config.EnableAlphaAPIFields,
+	}, {
+		name: "using debug when apifields stable",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{
+				Name: "my-task",
+			},
+			Debug: &v1.TaskRunDebug{
+				Breakpoint: []string{"onFailure"},
+			},
+		},
+		wantErr: apis.ErrGeneric("debug requires \"enable-api-fields\" feature gate to be \"alpha\" but it is \"stable\""),
+	}, {
+		name: "invalid breakpoint",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{
+				Name: "my-task",
+			},
+			Debug: &v1.TaskRunDebug{
+				Breakpoint: []string{"breakito"},
+			},
+		},
+		wantErr: apis.ErrInvalidValue("breakito is not a valid breakpoint. Available valid breakpoints include [onFailure]", "debug.breakpoint"),
+		wc:      config.EnableAlphaAPIFields,
+	}, {
+		name: "stepOverride disallowed without alpha feature gate",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{
+				Name: "foo",
+			},
+			StepOverrides: []v1.TaskRunStepOverride{{
+				Name: "foo",
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{corev1.ResourceMemory: corev1resources.MustParse("1Gi")},
+				},
+			}},
+		},
+		wantErr: apis.ErrGeneric("stepOverrides requires \"enable-api-fields\" feature gate to be \"alpha\" but it is \"stable\""),
+	}, {
+		name: "sidecarOverride disallowed without alpha feature gate",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{
+				Name: "foo",
+			},
+			SidecarOverrides: []v1.TaskRunSidecarOverride{{
+				Name: "foo",
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{corev1.ResourceMemory: corev1resources.MustParse("1Gi")},
+				},
+			}},
+		},
+		wantErr: apis.ErrGeneric("sidecarOverrides requires \"enable-api-fields\" feature gate to be \"alpha\" but it is \"stable\""),
+	}, {
+		name: "duplicate stepOverride names",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{Name: "task"},
+			StepOverrides: []v1.TaskRunStepOverride{{
+				Name: "foo",
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{corev1.ResourceMemory: corev1resources.MustParse("1Gi")},
+				},
+			}, {
+				Name: "foo",
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{corev1.ResourceMemory: corev1resources.MustParse("1Gi")},
+				},
+			}},
+		},
+		wantErr: apis.ErrMultipleOneOf("stepOverrides[1].name"),
+		wc:      config.EnableAlphaAPIFields,
+	}, {
+		name: "missing stepOverride names",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{Name: "task"},
+			StepOverrides: []v1.TaskRunStepOverride{{
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{corev1.ResourceMemory: corev1resources.MustParse("1Gi")},
+				},
+			}},
+		},
+		wantErr: apis.ErrMissingField("stepOverrides[0].name"),
+		wc:      config.EnableAlphaAPIFields,
+	}, {
+		name: "duplicate sidecarOverride names",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{Name: "task"},
+			SidecarOverrides: []v1.TaskRunSidecarOverride{{
+				Name: "bar",
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{corev1.ResourceMemory: corev1resources.MustParse("1Gi")},
+				},
+			}, {
+				Name: "bar",
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{corev1.ResourceMemory: corev1resources.MustParse("1Gi")},
+				},
+			}},
+		},
+		wantErr: apis.ErrMultipleOneOf("sidecarOverrides[1].name"),
+		wc:      config.EnableAlphaAPIFields,
+	}, {
+		name: "missing sidecarOverride names",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{Name: "task"},
+			SidecarOverrides: []v1.TaskRunSidecarOverride{{
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{corev1.ResourceMemory: corev1resources.MustParse("1Gi")},
+				},
+			}},
+		},
+		wantErr: apis.ErrMissingField("sidecarOverrides[0].name"),
+		wc:      config.EnableAlphaAPIFields,
+	}, {
+		name: "invalid both step-level (stepOverrides.resources) and task-level (spec.computeResources) resource requirements",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{Name: "task"},
+			StepOverrides: []v1.TaskRunStepOverride{{
+				Name: "stepOverride",
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{
+						corev1.ResourceMemory: corev1resources.MustParse("1Gi"),
+					},
+				},
+			}},
+			ComputeResources: &corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceMemory: corev1resources.MustParse("2Gi"),
+				},
+			},
+		},
+		wantErr: apis.ErrMultipleOneOf(
+			"stepOverrides.resources",
+			"computeResources",
+		),
+		wc: config.EnableAlphaAPIFields,
+	}, {
+		name: "computeResources disallowed without alpha feature gate",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{
+				Name: "foo",
+			},
+			ComputeResources: &corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceMemory: corev1resources.MustParse("2Gi"),
+				},
+			},
+		},
+		wantErr: apis.ErrGeneric("computeResources requires \"enable-api-fields\" feature gate to be \"alpha\" but it is \"stable\""),
+	}}
+
+	for _, ts := range tests {
+		t.Run(ts.name, func(t *testing.T) {
+			ctx := context.Background()
+			if ts.wc != nil {
+				ctx = ts.wc(ctx)
+			}
+			err := ts.spec.Validate(ctx)
+			if d := cmp.Diff(ts.wantErr.Error(), err.Error()); d != "" {
+				t.Error(diff.PrintWantGot(d))
+			}
+		})
+	}
+}
+
+func TestTaskRunSpec_Validate(t *testing.T) {
+	tests := []struct {
+		name string
+		spec v1.TaskRunSpec
+		wc   func(context.Context) context.Context
+	}{{
+		name: "taskspec without a taskRef",
+		spec: v1.TaskRunSpec{
+			TaskSpec: &v1.TaskSpec{
+				Steps: []v1.Step{{
+					Name:  "mystep",
+					Image: "myimage",
+				}},
+			},
+		},
+	}, {
+		name: "no timeout",
+		spec: v1.TaskRunSpec{
+			Timeout: &metav1.Duration{Duration: 0},
+			TaskSpec: &v1.TaskSpec{
+				Steps: []v1.Step{{
+					Name:  "mystep",
+					Image: "myimage",
+				}},
+			},
+		},
+	}, {
+		name: "parameters",
+		spec: v1.TaskRunSpec{
+			Timeout: &metav1.Duration{Duration: 0},
+			Params: []v1.Param{{
+				Name:  "name",
+				Value: *v1.NewArrayOrString("value"),
+			}},
+			TaskSpec: &v1.TaskSpec{
+				Steps: []v1.Step{{
+					Name:  "mystep",
+					Image: "myimage",
+				}},
+			},
+		},
+	}, {
+		name: "task spec with credentials.path variable",
+		spec: v1.TaskRunSpec{
+			TaskSpec: &v1.TaskSpec{
+				Steps: []v1.Step{{
+					Name:   "mystep",
+					Image:  "myimage",
+					Script: `echo "creds-init writes to $(credentials.path)"`,
+				}},
+			},
+		},
+	}, {
+		name: "valid task-level (spec.resources) resource requirements",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{Name: "task"},
+			ComputeResources: &corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceMemory: corev1resources.MustParse("2Gi"),
+				},
+			},
+		},
+		wc: config.EnableAlphaAPIFields,
+	}, {
+		name: "valid sidecar and task-level (spec.resources) resource requirements",
+		spec: v1.TaskRunSpec{
+			TaskRef: &v1.TaskRef{Name: "task"},
+			ComputeResources: &corev1.ResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceMemory: corev1resources.MustParse("2Gi"),
+				},
+			},
+			SidecarOverrides: []v1.TaskRunSidecarOverride{{
+				Name: "sidecar",
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{
+						corev1.ResourceMemory: corev1resources.MustParse("4Gi"),
+					},
+				},
+			}},
+		},
+		wc: config.EnableAlphaAPIFields,
+	}}
+
+	for _, ts := range tests {
+		t.Run(ts.name, func(t *testing.T) {
+			ctx := context.Background()
+			if ts.wc != nil {
+				ctx = ts.wc(ctx)
+			}
+			if err := ts.spec.Validate(ctx); err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
